dcb143589d44092bf1e01c2e9807eb4213cc783a
Sunao Tomita <sunao@jp.fujitsu.com>
2015-10-06 18:40:15 +0900

    今日はここまで

--- SoracomClient/Auth/AuthOperation.cs
+++ SoracomClient/Auth/AuthOperation.cs
@@ -17,7 +17,7 @@ namespace Soracom.Auth
         }
 
         /// <summary>
-        /// /auth<br/>
+        /// POST /auth<br/>
         /// Operator の認証を行う。<br/>
         /// 認証が成功した場合、API キー、オペレータ ID、 Token が返却される。<br/>
         /// 認証が必要なAPIのリクエストには API キーと Token をヘッダーに付与する必要がある。
@@ -26,8 +26,26 @@ namespace Soracom.Auth
         /// <returns></returns>
         public async Task<AuthResponse> Auth(AuthRequest request)
         {
-            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, new Uri(parent.BaseUri, "auth"));
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, new Uri(parent.BaseUri, "/auth"));
             message.Content = parent.ToStringContent(request);
+
+            AuthResponse response = await this.parent.SendRequest<AuthResponse>(message);
+
+            return response;
+        }
+
+        /// <summary>
+        /// /auth/password_reset_token/issue<br/>
+        /// パスワードリセット確認用のワンタイムトークンをメールで送付する
+        /// </summary>
+        /// <param name="email">登録済みメールアドレス</param>
+        /// <returns></returns>
+        public async Task<AuthResponse> Auth(string email)
+        {
+            UriBuilder builder = new UriBuilder(new Uri(parent.BaseUri, "/auth/password_reset_token/issue"));
+            builder.Query = "email=" + email;
+
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, builder.Uri);
 
             AuthResponse response = await this.parent.SendRequest<AuthResponse>(message);
 
